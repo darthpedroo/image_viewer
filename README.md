e7a08c2e50744882e07380553f072aa83d350724
darthpedroo <pedrovillarino06@gmail.com>
2026-07-02 14:39:40 -0300

    image viewer displays an image

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Image viewer
 
-A ppm image viewer made in C.
+A ppm image viewer made in C. This project was made following [This Video](!https://www.youtube.com/watch?v=sItRLFjbqvo).
+This image viewer can only open .ppm files.
 
-[Source](!https://www.youtube.com/watch?v=sItRLFjbqvo)
+## How to run
+Run `build.sh` to compile the project and to run it use this command:
+
+```bash
+cat image.ppm | ./build/image_viewer
+```
+
+## Gallery
+
+![Image](/assets/demo.png)
--- image_viewer.c
+++ image_viewer.c
@@ -1,4 +1,7 @@
+#include <SDL2/SDL_events.h>
 #include <SDL2/SDL_rect.h>
+#include <SDL2/SDL_stdinc.h>
+#include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_video.h>
 #include <SDL3/SDL_pixels.h>
 #include <SDL3/SDL_stdinc.h>
@@ -6,33 +9,52 @@
 #include <SDL2/SDL.h>
 
 int main(){
-    const int width = 800;
-    const int height = 800;
+    FILE *in = stdin;
+    char *pthrowaway = calloc(1000,sizeof(char));
+    // Read first line (specifier P3 or P6 - ignore here)
+    fgets(pthrowaway,1000, in); 
+    // Read second line (comment)
+    fgets(pthrowaway,1000, in); 
+    // Read third line (dimensions: width / height)
+    char *pdimensions = calloc(1000,sizeof(char));
+    fgets(pdimensions,1000, in); 
+    // Read fourth line (max color value - ignore it!)
+    fgets(pthrowaway,1000, in); 
+    // Free memory
+    free(pthrowaway);
 
-    printf("Hello world\n");
-    SDL_Window *pwindow = SDL_CreateWindow("¡LOL!",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,width,height,0);
+    // Parse dimensions
+    int width = 0;
+    int height = 0;
+    sscanf(pdimensions,"%d %d\n", &width, &height); //Beautiful function! Agarra un char, y lo formatea a lo que necesitemos nosotros.
+    free(pdimensions);
+    printf("width=%d, height=%d\n", width,height);
+    SDL_Window *pwindow = SDL_CreateWindow("Image Viewer",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,width,height,0);
     SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
-
-    Uint8 r,g,b;
-    r=0x00;
-    g=0x00;
-    b=0xFF;
-
-    Uint32 color = SDL_MapRGB(psurface->format,r,g,b);
-    Uint32 color_inner = SDL_MapRGB(psurface->format,0xFF,0x00,0x00);
-    
-    for (int x = 0; x < width;x++){
-        for (int y = 0; y < height; y++){
-        if (x % 2 == 0){
-            SDL_Rect pixel = (SDL_Rect){x,y,1,1};
+    Uint32 color =0;
+    SDL_Rect pixel = (SDL_Rect){0,0,1,1};
+    for (int y = 0; y < height;y++){
+        for (int x = 0; x < width; x++){
+            Uint8 r,g,b;
+            r=(char) getchar();
+            g=(char) getchar();
+            b=(char) getchar();
+            color = SDL_MapRGB(psurface->format,r,g,b);
+            pixel.x=x;
+            pixel.y=y;
             SDL_FillRect(psurface, &pixel, color);
-        }else {
-            SDL_Rect pixel = (SDL_Rect){x,y,1,1};
-            SDL_FillRect(psurface, &pixel, color_inner);
         }
         }
-    }
+    int app_running =1;
     SDL_UpdateWindowSurface(pwindow);
-    SDL_Delay(3000);
+    while(app_running){
+        SDL_Event event;
+        while(SDL_PollEvent(&event)){
+            if(event.type == SDL_QUIT){
+                app_running=0;
+            }
+        }
+        SDL_Delay(100);
+    }
     return 1;
 }
